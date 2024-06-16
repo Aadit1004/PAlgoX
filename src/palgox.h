42e1c500050868c1616d09ce9107e302cd66f315
Aadit Rao <aaditrao10@gmail.com>
2024-06-15 19:59:19 -0700

    updated header file for matx and vecx

--- src/palgox.h
+++ src/palgox.h
@@ -44,21 +44,64 @@ namespace palgox {
         [[nodiscard]] int getNumCols() const;
 
         [[nodiscard]] int getValue(int row, int col) const;
+
+        // TODO functions:
+        // TODO functions:
+        // TODO functions:
+
+        bool andMap(bool (*operation)(int)) const;
+
+        bool orMap(bool (*operation)(int)) const;
+
     };
 
     class palgox_vecx {
     private:
         std::vector<int> m_data;
         int m_numElems;
+
+        bool isSameSize(const palgox_vecx* other_vecx) const;
     public:
         explicit palgox_vecx(const std::vector<int>& input_data);
+
+        explicit palgox_vecx(int size, int (*operation)(int));
+
         bool isEqual(const palgox_vecx* other_vecx) const;
 
         [[nodiscard]] int getNumElems() const;
 
-        [[nodiscard]] int getValue(const int idx) const;
+        [[nodiscard]] int getValue(int idx) const;
 
-        // map, foldlm filter funcs?
+        void applyOperation(int (*operation)(int, int));
+
+        void addVecx(const palgox_vecx* other_vecx);
+
+        void subVecx(const palgox_vecx* other_vecx);
+
+        void quickSort();
+
+        void mergeSort();
+
+        int search(int value);
+
+        palgox_vecx* filter(bool (*operation)(int)) const;
+
+        bool andMap(bool (*operation)(int)) const;
+
+        bool orMap(bool (*operation)(int)) const;
+
+        int reduceLeft(int init, int (*operation)(int, int)) const;
+
+        int reduceRight(int init, int (*operation)(int, int)) const;
+
+        [[nodiscard]] int findMin() const;
+
+        [[nodiscard]] int findMax() const;
+
+        [[nodiscard]] palgox_vecx* prefixSum() const;
+
+        [[nodiscard]] palgox_vecx* suffixSum() const;
+
     };
 
     class palgox_graphx {
